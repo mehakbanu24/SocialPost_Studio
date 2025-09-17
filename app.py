05be191d5e8a122141da3c46ea88cb83053a3285
Mehak Banu <99721744+mehakbanu24@users.noreply.github.com>
2025-09-17 09:28:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import streamlit as st
 from flask import Flask, request, redirect, url_for, render_template_string, send_file
 import requests
 from bs4 import BeautifulSoup
@@ -212,5 +211,6 @@ def download_image():
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0", port=5001,use_reloader=False)
+
 
 
